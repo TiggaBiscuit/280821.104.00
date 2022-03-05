1d7591047055b6d0d19bdf03d3a7106c2e29c213
Dan Agnew <dan.agnew@outlook.co.nz>
2022-03-05 18:09:03 +1300

    Removal of Modernizer
    Removed Modernizer Framework.
    Added JQuery.
    Build a new NavBar based on https://codepen.io/tonkec/pen/gryZmg

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
+$(document).ready(function() {
+
+    function toggleSidebar() {
+      $(".button").toggleClass("active");
+      $("main").toggleClass("move-to-left");
+      $(".sidebar-item").toggleClass("active");
+    }
+  
+    $(".button").on("click tap", function() {
+      toggleSidebar();
+    });
+  
+    $(document).keyup(function(e) {
+      if (e.keyCode === 27) {
+        toggleSidebar();
+      }
+    });
+  
   });
